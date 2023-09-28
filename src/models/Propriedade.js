3f34a70c95c7e1fc6df8045584fe53ae8a0e8f09
João Vitor <joaovitorreal@outlook.com>
2023-09-28 17:33:34 -0300

    Criação da model Propriedade

---
+++ src/models/Propriedade.js
@@ -0,0 +1,21 @@
+import { DataTypes } from "sequelize";
+import db from "../config/database.js";
+
+const Propriedade = db.define("usuario", {
+    "idPropriedade": {
+        type: DataTypes.BIGINT, 
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    "nomePropriedade": {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    "cadastroRural": {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+export default Propriedade;
