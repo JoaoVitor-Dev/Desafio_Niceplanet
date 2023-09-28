335073fad31694379b3f06fc28b06c9b8b36f760
João Vitor <joaovitorreal@outlook.com>
2023-09-28 16:08:16 -0300

    Criação do model Produtor

---
+++ src/models/Produtor.js
@@ -0,0 +1,21 @@
+import { DataTypes } from "sequelize";
+import db from "../config/database.js";
+
+const Produtor = db.define("produtor", {
+    "idProdutor": {
+        type: DataTypes.BIGINT, 
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    "nomeProdutor": {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    "cpfprodutor": {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+export default Produtor;
