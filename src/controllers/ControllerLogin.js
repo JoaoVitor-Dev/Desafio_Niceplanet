645eb7d18f39219de8c0058bec0faea271a440b7
João Vitor <joaovitorreal@outlook.com>
2023-10-02 07:47:43 -0300

    Validação de login finalizada, geração de token para login válido implementado

--- src/controllers/ControllerLogin.js
+++ src/controllers/ControllerLogin.js
@@ -1,25 +1,39 @@
 import Usuario from "../models/Usuario.js";
+import bcryptjs from "bcryptjs";
+import Jwt from "jsonwebtoken";
 
 export default new class ControllerLogin {
     async login(req, res) {
 
-        //Verificando se o usuário informado na requisição existe no banco de dados
         const usuario = await Usuario.findOne({
-            attributes: ["nomeUsuario"],
+            attributes: ["nomeUsuario", "senhaUsuario"],
             where:{
-                nomeUsuario: req.body.nomeUsuario
+                nomeUsuario: req.body.nomeUsuario,
             }
         });
 
+        //Se o usuário não existir, retorna um json com a mensagem de erro
         if (!usuario) return res.json({
             mensagem: "Erro! Usuário ou senha incorretos",
         });
 
+        //Verificando se a senha passada na requisição é igual a senha hash do usuário
+        if(!(await bcryptjs.compare(req.body.senhaUsuario, usuario.senhaUsuario))){
+            return res.json({
+                mensagem: "Erro: Usuário ou senha incorretos!"
+            });
+        }
+
+        //gerar token
+        let token = Jwt.sign({id: usuario.idUsuario}, process.env.SECRET, {
+            
+            //Tempo de expiração do token
+            expiresIn: "2d"
+        });
         
         res.json({
-            error: false,
             mensagem: "Login realizado com sucesso!",
-            
+            token
         })
     }
 }
